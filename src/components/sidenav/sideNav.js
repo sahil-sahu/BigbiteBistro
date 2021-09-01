bd86cf17620594bd42f52d6b181535df15fbe279
sahil-sahu <sahilku2003@gmail.com>
2021-09-02 00:35:51 +0530

    sidenav is now cool

--- src/components/sidenav/sideNav.js
+++ src/components/sidenav/sideNav.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './sideNav.css';
-import insta from './assets/insta.svg';
-import fb from './assets/fb.svg';
-import wb from './assets/whatsapp.svg';
-import phone from './assets/phone.png';
+import logo from '../../logo.png';
+// import insta from './assets/insta.svg';
+// import fb from './assets/fb.svg';
+// import wb from './assets/whatsapp.svg';
+// import phone from './assets/phone.png';
 // import Swiper from 'https://unpkg.com/swiper/swiper-bundle.min.js';
 // importScripts('https://unpkg.com/swiper/swiper-bundle.min.js');
 
@@ -11,22 +12,22 @@ function Nav() {
     return(
         <>
         <div id="sidenav" class="sidenav">
-         <h2>Bistro</h2>
+         <h2><img src={logo} alt="" /></h2>
           <ul>
             <li><a href="menu">Menu</a></li>
-            <li><a href="https://www.zomato.com/bhubaneswar/big-bite-bistro-jayadev-vihar-bhubaneshwar">Zomato</a></li>
-            <li><a href="https://www.swiggy.com/restaurants/big-bite-bistro-sahid-nagar-bhubaneswar-390902">Swiggy</a></li>
             <li><a href="reserve">Reserve</a></li>
-            <li><a href="https://goo.gl/maps/zjziJQagLez1FcsMA">Maps</a></li>
+            <li><a target="_blank" href="https://www.zomato.com/bhubaneswar/big-bite-bistro-jayadev-vihar-bhubaneshwar">Zomato</a></li>
+            <li><a target="_blank" href="https://www.swiggy.com/restaurants/big-bite-bistro-sahid-nagar-bhubaneswar-390902">Swiggy</a></li>
+            <li><a target="_blank" href="https://goo.gl/maps/zjziJQagLez1FcsMA">Maps</a></li>
             <li><a href="#footer">Contacts</a></li>
           </ul>
         </div>
         <div className="topnav" id="topnav">
          <ul className="nav-content">
-            <li><a href="https://www.instagram.com/big.bitebistro/"><img src={insta} alt="" /></a></li>
-            <li><a href="https://www.facebook.com/bigbitebistro.bbsr/"><img src={fb} alt="" /></a></li>
-            <li><a href="https://wa.me/message/HVL7SEWYXKZTK1"><img src={wb} alt="" /></a></li>
-            <li><a href="tel:7972238279"><img src={phone} alt="" /></a></li>
+            <li><a target="_blank" href="https://www.instagram.com/big.bitebistro/"><i class="fab fa-instagram"></i></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/bigbitebistro.bbsr/"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a target="_blank" href="https://wa.me/message/HVL7SEWYXKZTK1"><i class="fab fa-whatsapp"></i></a></li>
+            <li><a href="tel:7972238279"><i class="fas fa-phone-alt"></i></a></li>
          </ul>
         </div>
         </>
